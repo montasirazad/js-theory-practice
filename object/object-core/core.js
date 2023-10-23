97f040a932e2125c5252aa0b6a88cf497992a6ff
Montasir Azad <montasirazad@gmail.com>
2023-10-23 18:13:58 +0600

    object detail

---
+++ object/object-core/core.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+const person = {
+    name: 'john',
+    age: 10,
+    getName: () => {
+        console.log(5);
+    }
+}
+
+const text = new String('bangladesh')
+
+person.getName()
+
+console.dir(text);
