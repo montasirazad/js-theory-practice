c4c2788728f3a54e514eedc52d3225c47fb0ef3d
Montasir Azad <montasirazad@gmail.com>
2023-11-21 15:56:26 +0600

    loop

--- loop/loop-1/loop1.js
+++ loop/loop-1/loop1.js
@@ -2,14 +2,28 @@ const text = 'Bangladesh'
 let possibleCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 let output = '';
-for (let i = 1; i <= 4; i++) {
-    let randomCharacter = possibleCharacter.charAt(Math.floor(Math.random() * possibleCharacter.length))
-    output += randomCharacter
-}
-console.log(possibleCharacter[0]);
+// for (let i = 1; i <= 4; i++) {
+//     let randomCharacter = possibleCharacter.charAt(Math.floor(Math.random() * possibleCharacter.length))
+//     output += randomCharacter
+// }
+// console.log(possibleCharacter[0]);
 
-const test = (a, b) => {
-    return c = [a, b]
+// const test = (a, b) => {
+//     return c = [a, b]
+// }
+// test(5)
+// console.log(test(3, 5));
+
+// for (let x of text) {
+//      output +=x
+
+// }
+// Reverse a string using js
+
+for (let i = text.length-1; i >=0; i--) {
+     const element = text[i];
+     output += element
 }
-test(5)
-console.log(test(3, 5));
+
+
+console.log(output);
