5cab9141bb0e5e5ef6ba8afe65832714939bc6e8
Montasir Azad <montasirazad@gmail.com>
2023-11-29 11:29:58 +0600

    closure js

--- Clouser/clouserMain.js
+++ Clouser/clouserMain.js
@@ -4,5 +4,13 @@ var sum = function () {
     return num1 + num2;
 }
 
-console.log(sum());
-console.dir(sum);
+function bankAccount(initialBalance){
+    var balance = initialBalance;
+    return function(){
+        return balance
+    }
+}
+
+var account = bankAccount(10000)
+
+console.dir(document);
