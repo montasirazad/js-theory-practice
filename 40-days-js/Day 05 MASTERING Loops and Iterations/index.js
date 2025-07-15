12ec676939e0fb891977aff693573d74ee7538f3
Montasir Azad <montasirazad@gmail.com>
2025-07-15 18:15:32 +0600

    loop practice folder structure change

--- 40-days-js/Day 05 MASTERING Loops and Iterations/index.js
+++
@@ -1,7 +0,0 @@
-// Nested loop
-
-for (let i = 1; i <= 3; i++) {
-  for (let j = 1; j <= 3; j++) {
-    
-  }
-}
---
+++ 40-days-js/Day05_Loops_and_Iterations/index.js
@@ -0,0 +1,9 @@
+// Nested loop
+
+for (let i = 1; i <= 3; i++) {
+  let text = "";
+  for (let j = i; j = i; j++) {
+    text += "*";
+}
+console.log(text);
+}
