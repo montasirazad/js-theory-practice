4ae9ad70b02ecaa7ea2956dc086566a57355280e
Montasir Azad <montasirazad@gmail.com>
2025-07-21 12:13:55 +0600

    practicing this keyword

---
+++ 40-days-js/Day_16_this_keyword/index.js
@@ -0,0 +1,23 @@
+// 'use strict';
+
+const thisKeyword = () => console.log(this);
+
+thisKeyword();
+
+const person = {
+  name: "alex",
+  age: 10,
+  myFunction: function () {
+    return () => `${this.name} is ${this.age} years old`
+  },
+};
+
+const msg = person.myFunction()
+
+console.log(msg());
+
+function testStrict(){
+    return `this inside the function is ${this}`
+}
+
+console.log(testStrict());
