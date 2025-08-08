9d66185e0d714a5fdde1555d4c58394f0224df9c
Montasir Azad <montasirazad@gmail.com>
2025-08-08 18:04:24 +0600

    practicing this keyword

--- 40-days-js/Day_13_this_keyword/index.js
+++ 40-days-js/Day_13_this_keyword/index.js
@@ -1,26 +1,43 @@
-'use strict';
+// 'use strict';
 
-const thisKeyword = () => console.log(this);
+// const thisKeyword = () => console.log(this);
 
-thisKeyword();
+// thisKeyword();
 
-const person = {
-  name: "alex",
-  age: 10,
-  myFunction: function () {
-    return () => `${this.name} is ${this.age} years old`
-  },
-};
+// const person = {
+//   name: "alex",
+//   age: 10,
+//   myFunction: function () {
+//     return () => `${this.name} is ${this.age} years old`
+//   },
+// };
 
-const msg = person.myFunction()
+// const msg = person.myFunction()
 
-console.log(msg());
+// console.log(msg());
 
-function testStrict(){
-    return `this inside the function is ${this}`
-}
+// function testStrict(){
+//     return `this inside the function is ${this}`
+// }
 
-console.log(testStrict());
+// console.log(testStrict());
 
 // the call method
 
+/**Practice */
+
+console.log(this);
+
+// inside of object -- Implicit binding
+
+const employee = {
+  id: 1,
+  firstName: "Alex",
+  lastName: "b",
+  returnThis: function () {
+    return this;
+  },
+};
+
+
+console.log(employee.returnThis());
