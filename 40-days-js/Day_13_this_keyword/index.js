1fca61b7f748f6103b1da8b370b59064832546fd
Montasir Azad <montasirazad@gmail.com>
2025-08-09 18:04:31 +0600

    day 14 js error handling

--- 40-days-js/Day_13_this_keyword/index.js
+++ 40-days-js/Day_13_this_keyword/index.js
@@ -37,7 +37,39 @@ const employee = {
   returnThis: function () {
     return this;
   },
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
 };
 
-
 console.log(employee.returnThis());
+console.log("Getting full name", employee.getFullName());
+
+const tom = {
+  name: "Tom",
+  age: 7,
+};
+
+const jerry = {
+  name: "jerry",
+  age: 3,
+};
+
+function greetMe(obj) {
+  obj.logMessage = function () {
+    console.log(`${this.name} is ${this.age} years old`);
+  };
+}
+console.log(tom);
+greetMe(tom)
+
+tom.logMessage()
+
+// inside function
+
+function sayName() {
+  console.log(this);
+}
+sayName()
+
+// arrow function
