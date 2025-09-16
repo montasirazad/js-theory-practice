50adc5555230db8459e84cb38ebebdc6f37a7ab6
Montasir Azad <montasirazad@gmail.com>
2025-09-16 16:14:47 +0600

    DOM

---
+++ 40-days-js/Day_17-to-21_all_dom/index.js
@@ -0,0 +1,5 @@
+let titleElem = document.getElementById("heading");
+let paragraphElem = document.getElementsByClassName("line");
+
+let para = document.querySelector('p.line')
+console.log(document.querySelectorAll('p.line'));
