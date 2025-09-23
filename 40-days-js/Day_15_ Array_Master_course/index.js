53a749b261b0ac45583ffd3775ab94f8460437bf
Montasir Azad <montasirazad@gmail.com>
2025-09-23 19:49:49 +0600

    array problem practice

--- 40-days-js/Day_15_ Array_Master_course/index.js
+++ 40-days-js/Day_15_ Array_Master_course/index.js
@@ -1,25 +1,33 @@
-const myArr = new Array(1, 3);
+const myArr = new Array(1, 3, 2);
 
-console.log(myArr);
+//console.log(myArr);
 
 const newArr = ["aa", "bb", "cc", "dd", "ee", "ff"];
-console.log('newArr length',newArr.length);
-for (i = 0; i <= newArr.length-1; i++) {
-    console.log(newArr[i]);
-}
+//console.log('newArr length',newArr.length);
+// for (i = 0; i <= newArr.length-1; i++) {
+//     console.log(newArr[i]);
+// }
 
-console.log(newArr.join('<-->'));
+// console.log(newArr.join('<-->'));
 
-newArr.push('gg')
-console.log(newArr);
+// newArr.push('gg')
+// console.log(newArr);
 
-newArr.pop()
+// newArr.pop()
 
-console.log(newArr);
+// console.log(newArr);
 
-newArr.shift()
-console.log(newArr);
+// newArr.shift()
+// console.log(newArr);
 
+// newArr.unshift('aa')
+// console.log(newArr);
 
-newArr.unshift('aa')
-console.log(newArr);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.find((value, index, obj) =>
+  console.log(`Value:${value} \n index:${index} \n obj:${obj}`)
+);
+
+const result = fruits.includes("carrot");
+
+console.log(result);
