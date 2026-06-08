029fe88fdc8a60ec313ef0332a9843960379f0b2
Montasir Azad <montasirazad@gmail.com>
2026-06-09 02:30:26 +0600

    revising object 2nd  commit

---
+++ js-closures/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  let a = 4;
+  return a * a;
+}
+
+const value = myFunction();
+console.log(value);
---
+++ regx/app.js
@@ -0,0 +1,7 @@
+let text = 'visit w3schools'
+
+const n = text.search(/w3schools/i)
+const m = text.replace(/w3schools/i,'google')
+console.log(n);
+console.log(m);
+console.log(text);
