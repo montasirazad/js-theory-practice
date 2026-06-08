4f4cc9336a4a073638ef1313d9e1d8a0f14cfda2
Montasir Azad <montasirazad@gmail.com>
2026-06-09 02:29:51 +0600

    revising object

--- basic-js/script.js
+++ basic-js/script.js
@@ -1,5 +1,24 @@
-function Person(first, last, age, eyecolor) {
-  this.first = first;
-}
-const txt = 'hello'
- 
+// function Person(first, last, age, eyecolor) {
+//   this.first = first;
+// }
+// const txt = 'hello'
+
+const car = {
+  name: "Fiat",
+  model: 500,
+  weight: "850 kg",
+  color: "white",
+  start: function () {
+    console.log("car has started");
+  },
+  drive: function () {
+    console.log("car is driving");
+  },
+  details: function () {
+    return "This " + this.name + " car is " + this.weight;
+  },
+};
+
+console.log(car.details())
+
+// if value is assigned directly it is called literal syntax
