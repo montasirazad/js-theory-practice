51e678e41988acddf685bcd1ec7e5b6fbcc86e16
Montasir Azad <montasirazad@gmail.com>
2026-06-28 03:13:55 +0600

    practice array & loop

--- 40-days-js/Day_15_ Array_Master_course/index.js
+++ 40-days-js/Day_15_ Array_Master_course/index.js
@@ -3,6 +3,7 @@ const myArr = new Array(1, 3, 2);
 const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const newArr = ["aa", "bb", "cc", "dd", "ee", "ff"];
 const fruits = ["Banana", "Orange", "Apple", "Mango", "lemon"];
+let sum = 0;
 
 //console.log('newArr length',newArr.length);
 // for (i = 0; i <= newArr.length-1; i++) {
@@ -37,16 +38,17 @@ const fruits = ["Banana", "Orange", "Apple", "Mango", "lemon"];
 // console.log(sliced);
 // console.log(fruits);
 
- //newArr.push("1");
- //console.log("push=", newArr);
- //newArr.pop();
- //console.log("pop=", newArr);
- //newArr.shift();
- //console.log("shift=", newArr);
+//newArr.push("1");
+//console.log("push=", newArr);
+//newArr.pop();
+//console.log("pop=", newArr);
+//newArr.shift();
+//console.log("shift=", newArr);
 
+//  numArr.sort((a,b)=>b-a)
+//  console.log(numArr);
 
- numArr.sort((a,b)=>b-a)
- console.log(numArr);
-
- const stars= new Array(5).fill('⭐')
- console.log(stars);
+//  const stars= new Array(5).fill('⭐')
+//  console.log(new Array(9).fill(5));
+//  const [one,two] = numArr
+//  console.log(one,two);
