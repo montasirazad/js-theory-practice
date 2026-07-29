e9ca3c0c7f31c4afd8542cebca4c2d0e4478d98b
Montasir Azad <montasirazad@gmail.com>
2026-07-30 01:48:20 +0600

    practice

---
+++ 40-days-js/js-prototype/index.js
@@ -0,0 +1,29 @@
+// let person = {};
+// person.name = "A";
+// person.age = 10;
+
+// person.eat = function () {
+//   console.log(`person is eating`);
+// };
+// person["sleep"] = function () {
+//   console.log(`person is sleeping`);
+// };
+
+// console.log(person);
+
+// function Person(name, age) {
+//   let person = {};
+//   person.name = name;
+//   person.age = age;
+//   person.eat = function () {
+//     console.log(`person is eating`);
+//   };
+//   person["sleep"] = function () {
+//     console.log(`person is sleeping`);
+//   };
+//   return person;
+// }
+
+// const john = Person("john", 10);
+// const doe = Person("doe", 30);
+// console.log(john);
