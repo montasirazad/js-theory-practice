dfcc6dbddcc1ddf5fe6e87cbfc401c912f838460
Montasir Azad <montasirazad@gmail.com>
2026-07-30 20:09:45 +0600

    object prototype

--- 40-days-js/js-prototype/index.js
+++ 40-days-js/js-prototype/index.js
@@ -1,16 +1,35 @@
-class Food {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
-  }
-  details() {
-    console.log(`Name: ${this.name} -- Price: ${this.price}`);
-  }
-}
+// construction function
 
-const pasta = new Food("pasta", 300);
-console.log(pasta);
-pasta.details();
+function Car(model, year, color, owner) {
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.owner = owner;
+  this.wheelNumber = function () {
+    return 4;
+  };
+}
+function name(params) {
+  
+}
+const bmw = new Car("bmw", 2023, "blue");
+console.log(bmw, bmw.wheelNumber());
+
+// composite pattern
+
+// class Food {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+//   details() {
+//     console.log(`Name: ${this.name} -- Price: ${this.price}`);
+//   }
+// }
+
+// const pasta = new Food("pasta", 300);
+// console.log(pasta);
+// pasta.details();
 
 // let person = {};
 // person.name = "A";
