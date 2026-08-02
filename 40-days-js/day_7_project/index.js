0cb9b133b745e061b8bb589dbd0e0e0a1d2f24e1
Montasir Azad <montasirazad@gmail.com>
2026-08-02 17:28:03 +0600

    40 days of js project-day 6 - rock,scissor,paper project

---
+++ 40-days-js/day_7_project/index.js
@@ -0,0 +1,17 @@
+function rockPaperScissorsGame() {
+  const userChoicePrompt = prompt("Enter rock/paper/scissors");
+  let userChoice = userChoicePrompt.toLocaleLowerCase();
+
+  let computerChoice;
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  switch (randomNumber) {
+    case 1: computerChoice ==='rock'
+        
+        break;
+  
+    default:
+        break;
+  }
+}
+
+// rockPaperScissorsGame();
