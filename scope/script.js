b3d56a40d5b9c436f1100e188b577a1fa00e45b1
Montasir Azad <montasirazad@gmail.com>
2026-09-15 18:34:42 +0600

    practice let,var,const

--- scope/script.js
+++ scope/script.js
@@ -1,1 +1,16 @@
-console.log(200);
+var x = 23;
+
+const myFunction = () => {
+  var a = 10;
+  console.log("from myFunction value of a", a);
+  console.log("from myFunction value of x", x);
+};
+// myFunction()
+// console.log('value of a: ',a);
+
+let b = 10;
+function test() {
+  console.log("from function", b);
+}
+test();
+console.log("from outside", b);
